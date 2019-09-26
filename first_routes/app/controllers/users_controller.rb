13d0d0ddabe25246e2210ed74af7726af2580343
Claudius <claudius.solomon@gmail.com>
2019-09-25 19:31:27 -0700

    Metaprogramming and rails routes

---
+++ first_routes/app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def index
+    render plain: "Im in the index action!"
+  end
+
+  def show
+    render json: params
+  end
+
+  def create
+    render json: params
+  end
+
+end
---
+++ first_routes/db/migrate/20190925230617_change_users_columns.rb
@@ -0,0 +1,10 @@
+class ChangeUsersColumns < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :name
+    remove_column :users, :email
+
+  
+
+
+  end
+end
